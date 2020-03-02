b5ceb60478360d077983e0a0926fc16fedf76a8d
Laurens-Post <contact@lphosting.nl>
2020-03-02 12:20:45 +0100

    Setup Model + Controller with fields

---
+++ app/Http/Controllers/FormController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Form;
+
+class FormController extends Controller
+{
+    public function index(){
+
+        $forms = Form::all();
+
+        return view('forms.index',compact('forms'));
+    }
+
+    public function create(){
+        return view('devices.create');
+    }
+
+    public function storeForm(){
+
+        $form = new Form();
+
+        $form->country = request('country');
+        $form->education = request('education');
+        $form->kind_of_education = request('kind_of_education');
+        $form->name_school = request('name_school');
+        $form->description = request('description');
+        $form->date = request('date');
+        $form->diploma = request('diploma');
+
+        $form->save();
+
+        return redirect('/form');
+
+    }
+
+}
