bc75aeb8bce478c90c7b542127b4980aad33fc58
Laurens-Post <contact@lphosting.nl>
2020-03-02 17:30:02 +0100

    Edited the education dropdown to have a search + dropdown

--- resources/views/forms/create.blade.php
+++ resources/views/forms/create.blade.php
@@ -3,6 +3,7 @@
 <head>
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet" />
     <style>
         body {font-family: Montserrat, Arial, sans-serif;}
         * {box-sizing: border-box;}
@@ -42,7 +43,7 @@
 </head>
 <body>
 
-<h1>Grenzelooswerk Testformulier</h1>
+<h1>Opleidingsformulier</h1>
 
 <div class="container">
 
@@ -50,9 +51,8 @@
 
         {{ csrf_field() }}
 
-
         <div>
-            <label for="country">Land behaald opleiding</label>
+            <label for="country">Land waar je hebt gestudeerd</label>
             <div class="form-group">
                 <select name="country_id" id="country" class="form-control">
                     @foreach($countries as $id => $country)
@@ -64,7 +64,11 @@
             </div>
         </div>
         <div>
-            <label>Opleiding</label>
+            <label>Naam school</label>
+            <input type="text" name="name_school" placeholder="Wat is de naam van jou school?">
+        </div>
+        <div>
+            <label>Opleiding</label>*
             <div class="form-group">
                 <select name="education_id" id="education" class="form-control">
                     @foreach($educations as $id => $education)
@@ -77,15 +81,11 @@
         </div>
         <div>
             <label>Soort opleiding</label>
-            <input type="text" name="kind_of_education" placeholder="Kind of education">
-        </div>
-        <div>
-            <label>Naam school</label>
-            <input type="text" name="name_school" placeholder="Name school">
+            <input type="text" name="kind_of_education" placeholder="Wat is de naam van uw opleiding?">
         </div>
         <div>
             <label>Description</label>
-            <textarea name="description" placeholder="Description"></textarea>
+            <textarea name="description" placeholder="Geef hier een beschrijving"></textarea>
         </div>
         <div>
             <label>Datum</label>
@@ -106,6 +106,18 @@
     </form>
 
 </div>
+
+
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
+
+<script type="text/javascript">
+
+    $("#education").select2({
+        placeholder: "Kies opleiding",
+        allowClear: false
+    });
+</script>
 
 </body>
 </html>
