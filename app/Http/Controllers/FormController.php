da3ac6bb7ec5ac89465ae8229bdac41fdb2d43c9
Laurens-Post <contact@lphosting.nl>
2020-03-03 12:26:54 +0100

    Fixed the degrees to make it work

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Form;
 use App\Country;
 use App\Education;
+use App\Education_des;
 use App\EducationTypes;
 
 class FormController extends Controller
@@ -19,10 +20,12 @@ class FormController extends Controller
     }
 
     public function create(){
-        $educations = Education::all()->pluck('name', 'id')->prepend(trans('Selecteer opleiding'));
-        $type_educations = EducationTypes::all()->pluck('name', 'id')->prepend(trans('Geef de naam van je opleiding'));
+        $educations = Education::all()->pluck('name', 'id')->prepend(trans('Wat is het niveau van de opleiding'));
+        $educations_de = Education_des::all()->pluck('name', 'id')->prepend(trans('Wat is het niveau van de opleiding'));
+        $educations_fr = Education_fr::all()->pluck('name', 'id')->prepend(trans('Wat is het niveau van de opleiding'));
+        $type_educations = EducationTypes::all()->pluck('name', 'id')->prepend(trans('Vul hier de naam van de opleiding'));
         $countries = Country::all()->pluck('name', 'id')->prepend(trans('Selecteer land'));
-        return view('forms.create', compact('countries','educations', 'type_educations'));
+        return view('forms.create', compact('countries','educations', 'type_educations', 'educations_de', 'educations_fr'));
     }
 
     public function storeForm(){
@@ -30,16 +33,16 @@ class FormController extends Controller
         $forms = new Form();
 
         $forms->country = request('country');
+        $forms->name_school = request('name_school');
         $forms->education = request('education');
         $forms->kind_of_education = request('kind_of_education');
-        $forms->name_school = request('name_school');
         $forms->description = request('description');
         $forms->date = request('date');
         $forms->diploma = request('diploma');
 
         $forms->save();
 
-        return redirect('/form');
+        return redirect('/');
 
     }
 
--- resources/views/forms/create.blade.php
+++ resources/views/forms/create.blade.php
@@ -37,13 +37,79 @@
             border-radius: 8px;
             background-color: #efefef;
             padding: 20px;
+            width: 800px;
+            margin: 0 auto;
+        }
+
+        .headertitle {
+            margin: 1em auto;
+            width: 800px;
+        }
+
+        .select2.select2-container {
+            margin-top: 7px;
+            margin-bottom: 20px;
+            width: 100%!important;
+        }
+
+        #Nederland {
+            display: none;
+        }
+
+        #Nederland.show {
+            display: block;
+        }
+
+        #Duitsland {
+            display: none;
+        }
+
+        #Duitsland.show {
+            display: block;
+        }
+
+        #Frankrijk {
+            display: none;
+        }
+
+        #Frankrijk.show {
+            display: block;
+        }
+/*
+        #nameschool {
+            display: none;
+        }
+
+        #nameschool.show {
+            display: block;
+        }
+
+        #niveau {
+            display: none;
+        }
+
+        #niveau.show {
+            display: block;
+        }
+ */
+        .button {
+            background-color: #ed743e;
+            color: white;
+            padding: 12px 20px;
+            border: none;
+            font-weight: 500;
+            border-radius: 8px;
+            cursor: pointer;
+        }
+        .button:hover {
+            background-color: #f2895a;
         }
     </style>
 
 </head>
 <body>
 
-<h1>Opleidingsformulier</h1>
+<h1 class="headertitle">Opleiding toevoegen</h1>
 
 <div class="container">
 
@@ -54,80 +120,150 @@
         <div>
             <label for="country">Land waar je hebt gestudeerd</label>
             <div class="form-group">
-                <select name="country_id" id="country" class="form-control">
+                <select name="country" id="country" class="form-control">
                     @foreach($countries as $id => $country)
-                        <option value="{{ $id }}">
+                        <option id="{{ $id }}" value="{{ $country }}">
                             {{ $country }}
                         </option>
                     @endforeach
                 </select>
             </div>
         </div>
-        <div>
-            <label>Naam school</label>
-            <input type="text" name="name_school" placeholder="Wat is de naam van jou school?">
-        </div>
-        <div>
-            <label>Opleiding</label>*
-            <div class="form-group">
-                <select name="education_id" id="education" class="form-control">
-                    @foreach($educations as $id => $education)
-                        <option value="{{ $id }}">
-                            {{ $education }}
-                        </option>
-                    @endforeach
-                </select>
+
+        <div id="Nederland">
+            <div id="nameschool">
+                <label>Naam school</label>
+                <input type="text" name="name_school" placeholder="Wat is de naam van jou school?">
+            </div>
+            <div id="niveau">
+                <label>Niveau opleiding</label>*
+                <div class="form-group">
+                    <select name="education" id="education" class="form-control">
+                        @foreach($educations as $id => $education)
+                            <option value="{{ $education }}">
+                                {{ $education }}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
+            <div id="education_type">
+                <label>Soort opleiding</label>
+                <div class="form-group">
+                    <select name="kind_of_education" id="education_types" class="form-control">
+                        @foreach($type_educations as $id => $type_education)
+                            <option value="{{ $type_education }}">
+                                {{ $type_education }}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
+            <div id="description">
+                <label>Description</label>
+                <textarea name="description" placeholder="Geef hier een beschrijving"></textarea>
+            </div>
+            <div id="date">
+                <label>Datum opleiding gestart</label>
+                <input type="date" name="date" placeholder="Startdate">
+            </div>
+            <div id="date">
+                <label>Datum opleiding behaald</label>
+                <input type="date" name="date2" placeholder="Enddate">
+            </div>
+            <div id="present">
+                <input type="checkbox" name="stillbusy" value="now"> Ik ben nog bezig met deze opleiding
+            </div>
+            <br />
+            <div>
+                <label>Diploma</label>
+                <input type="radio" name="diploma" value="1"> Ja
+                <input type="radio" name="diploma" value="0" checked> Nee
+            </div>
+            <br />
+            <div>
+            <br/>
+                <input type="submit" value="Verstuur">
             </div>
         </div>
-        <div>
-            <label>Soort opleiding</label>
-            <div class="form-group">
-                <select name="type_education_id" id="education_types" class="form-control">
-                    @foreach($type_educations as $id => $type_education)
-                        <option value="{{ $id }}">
-                            {{ $type_education }}
-                        </option>
-                    @endforeach
-                </select>
+        <div id="Duitsland">
+            <div id="nameschool">
+                <label>Naam school</label>
+                <input type="text" name="name_school" placeholder="Wat is de naam van jou school?">
+            </div>
+            <div id="niveau">
+                <label>Niveau opleiding</label>*
+                <div class="form-group">
+                    <select name="education_des" id="education_des" class="form-control">
+                        @foreach($educations_de as $id => $education_des)
+                            <option value="{{ $education_des }}">
+                                {{ $education_des }}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
+            <div id="education_type">
+                <label>Soort opleiding</label>
+                <div class="form-group">
+                    <input type="text" name="kind_of_education" placeholder="Wat is de naam van de opleiding?">
+                </div>
+            </div>
+            <div id="description">
+                <label>Description</label>
+                <textarea name="description" placeholder="Geef hier een beschrijving"></textarea>
             </div>
         </div>
-        <div>
-            <label>Description</label>
-            <textarea name="description" placeholder="Geef hier een beschrijving"></textarea>
-        </div>
-        <div>
-            <label>Datum</label>
-            <input type="text" name="date" placeholder="Date">
-        </div>
-        <br />
-        <div>
-            <label>Diploma</label>
-            <input type="radio" name="diploma" value="1"> Ja
-            <input type="radio" name="diploma" value="0" checked> Nee
-        </div>
-        <div>
         <br/>
-            <input type="submit" value="Verstuur">
-
+        <input type="submit" value="Verstuur">
+        <div id="Frankrijk">
+            <div id="niveau">
+                <label>Niveau opleiding</label>*
+                <div class="form-group">
+                    <select name="education_des" id="education_des" class="form-control">
+                        @foreach($educations_fr as $id => $education_fr)
+                            <option value="{{ $education_fr }}">
+                                {{ $education_fr }}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
         </div>
-
     </form>
-
 </div>
 
 
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
 
-<script type="text/javascript">
+
+ <script type="text/javascript">
+     document.getElementById("country").onchange = function () {
+        if (this[this.selectedIndex].value === "Nederland") {
+            document.getElementById("Nederland").className = "show";
+        } else {
+            document.getElementById("Nederland").className = "";
+        }
+        if (this[this.selectedIndex].value === "Duitsland") {
+            document.getElementById("Duitsland").className = "show";
+        } else {
+            document.getElementById("Duitsland").className = "";
+        }
+         if (this[this.selectedIndex].value === "Frankrijk") {
+             document.getElementById("Frankrijk").className = "show";
+         } else {
+             document.getElementById("Frankrijk").className = "";
+         }
+    };
 
     $("#education").select2({
-        placeholder: "Kies opleiding",
+        placeholder: "Geef de naam van je opleiding",
         allowClear: false
     });
 
     $("#education_types").select2({
-        placeholder: "Geef de naam van je opleiding",
+        placeholder: "Soort opleiding",
         allowClear: false
     });
 </script>
